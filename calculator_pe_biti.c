023c8010f830f5b3f9dd3eedbb1d4f33e0040ae5
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2022-12-22 00:50:26 +0200

    am adaugat comentarii

--- calculator_pe_biti.c
+++ calculator_pe_biti.c
@@ -5,6 +5,7 @@ struct calculator {
     void (*function)(int, int *);
 };
 
+// functie pentru implementarea adunarii cu trunchiere la 4 biti
 void adunare(int poz_nr, int *numere) {
     int tr = 0, nr_biti = 0, i;
     /*nr_biti poate fi 0, 1, 2, 3 in functie de tr si bitii de pe pozitia pe
@@ -56,6 +57,10 @@ void xorul(int poz_nr, int *numere) {
             ok2 = 1;
         }
 
+        // ok1 si ok2 au valorile bitilor din pozitia i a celor doua numere
+        // implicate in operatie
+        // daca au aceeasi valoare bitul va fi 0
+        // daca au valori diferite bitul va fi 1
         if (ok1 == ok2) {
             numere[0] = numere[0] & (~(1 << i));
         } else {
@@ -64,18 +69,26 @@ void xorul(int poz_nr, int *numere) {
     }
 }
 
+// implementarea functiei de rotire la stanga a unui numar cu un bit
 void rotire_stanga_1_bit(int poz_nr, int *numere) {
-    int da = numere[0] & (1 << 3);        // bitul de pus pe pozitia 0
+    int ok = numere[0] & (1 << 3);        // bitul de pus pe pozitia 0
     numere[0] <<= 1;                      // muti la stanga cu o pozitie
     numere[0] = numere[0] & (~(1 << 4));  // setezi bitul de pe pozitia 4 pe 0
-    if (da) {                             // daca bitul e 1
-        numere[0] = numere[0] | 1;        // setezi bitu 0 pe 1
+    if (ok) {
+        // daca bitul e 1
+        numere[0] = numere[0] | 1;
+        // setezi bitu 0 pe 1
     } else {
+        // resetezi bitul 1 pe 0
         numere[0] = numere[0] & (~1);
     }
 }
 
+// implementarea functiei pentru rotirea la stanga cu un anumit numar de pozitii
 void rotire_stanga(int poz_nr, int *numere) {
+    // lucram pe 4 biti, deci orice rotire a numarului cu un nr de 4k biti va
+    // lasa numarul neschimbat
+    // concluzie: putem roti doar de un numar egal cu restul impartirii la 4
     numere[poz_nr] %= 4;
     while (numere[poz_nr]) {
         rotire_stanga_1_bit(poz_nr, numere);
@@ -84,7 +97,10 @@ void rotire_stanga(int poz_nr, int *numere) {
     }
 }
 
+// implementarea functiei de interschimbare
 void interschimbare(int poz_nr, int *numere) {
+    // cream in poz1 si poz2 numerele reprezentand pozitiile bitilor ce se vor
+    // interschimba
     int poz1 = 0, poz2 = 0;
     if (numere[poz_nr] & (1 << 3)) {
         poz1 = 2;
@@ -100,46 +116,33 @@ void interschimbare(int poz_nr, int *numere) {
         poz2++;
     }
 
+    // daca sunt pozitii diferite(in caz contrar interschimbarea este inutila)
     if (poz1 != poz2) {
-        int da1 = 0, da2 = 0;
+        // retinem valorile bitilor din pozitiile poz1 si poz2
+        int bit1 = 0, bit2 = 0;
+        if (numere[0] & (1 << (3 - poz1))) bit1 = 1;
 
-        if (numere[0] & (1 << (3 - poz1))) da1 = 1;
+        if (numere[0] & (1 << (3 - poz2))) bit2 = 1;
 
-        if (numere[0] & (1 << (3 - poz2))) da2 = 1;
-
-        // printf("da1:%d || da2:%d || poz1:%d || poz2:%d\n", da1, da2, poz1,
-        // poz2);
-
-        if (da1 != da2) {
-            if (da1 == 0) {
-                numere[0] = numere[0] |
-                            (1 << (3 - poz1));  // setezi bitu de pe poz1 pe 1
-                numere[0] =
-                    numere[0] &
-                    (~(1 << (3 - poz2)));  // resetezi bitu de pe poz2(pe 0)
+        // daca bitii sunt diferiti atunci are sens interschimbarea
+        if (bit1 != bit2) {
+            if (bit1 == 0) {
+                // daca primul era 0 il setam pe primul pe 1 si il resetam pe al
+                // doilea la 0
+                numere[0] = numere[0] | (1 << (3 - poz1));
+                numere[0] = numere[0] & (~(1 << (3 - poz2)));
             } else {
-                numere[0] = numere[0] |
-                            (1 << (3 - poz2));  // setezi bitu de pe poz1 pe 1
-                numere[0] =
-                    numere[0] &
-                    (~(1 << (3 - poz1)));  // resetezi bitu de pe poz2(pe 0)
+                // daca primul era 1 il resetam pe primul pe 0 si il setam pe al
+                // doilea pe 1
+                numere[0] = numere[0] | (1 << (3 - poz2));
+                numere[0] = numere[0] & (~(1 << (3 - poz1)));
             }
         }
     }
 }
 
-void afisare(int x) {
-    for (int i = 8; i >= 0; --i) {
-        if (x & (1 << i)) {
-            printf("1");
-        } else
-            printf("0");
-    }
-    // printf("\n");
-}
-
+// functie pentru calcularea operatiilor calculatorului
 void calculam(struct calculator *my_operations, int op, int *numere) {
-    // afisare(numere[0]);
     int i;
     for (i = 0; i < op; ++i) {
         my_operations[i].function(i + 1, numere);
@@ -147,34 +150,44 @@ void calculam(struct calculator *my_operations, int op, int *numere) {
 }
 
 int main() {
+    // numarul de operatii
     int op;
+    // variabila care va retine numerele si operatiile
     unsigned int a;
     scanf("%d%u", &op, &a);
-    struct calculator *oper = malloc(op * sizeof(struct calculator *));
+
+    // strcutura ce va retine operatiile
+    struct calculator *operatii = malloc(op * sizeof(struct calculator *));
+
+    // vectorul ce va retine numerele pe care se executa operatiile
     int numere[5], i;
-    // int operatii[5];
+
+    // adaugam numerele si operatiile in tablouri
     for (i = op; i > 0; --i) {
         numere[i] = a % 16;
         a /= 16;
-        // operatii[i - 1] = a % 4;
         switch (a % 4) {
             case 0:
-                oper[i - 1].function = adunare;
+                operatii[i - 1].function = adunare;
                 break;
             case 1:
-                oper[i - 1].function = interschimbare;
+                operatii[i - 1].function = interschimbare;
                 break;
             case 2:
-                oper[i - 1].function = rotire_stanga;
+                operatii[i - 1].function = rotire_stanga;
                 break;
             case 3:
-                oper[i - 1].function = xorul;
+                operatii[i - 1].function = xorul;
                 break;
         }
         a /= 4;
     }
     numere[0] = a % 16;
-    calculam(oper, op, numere);
+
+    // functia pentru calculul operatiilor
+    calculam(operatii, op, numere);
+
+    // afisam numarul obtinut
     printf("%d", numere[0]);
 
     return 0;
