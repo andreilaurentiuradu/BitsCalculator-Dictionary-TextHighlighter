97b4884192a03f02335b6dc9399c9bd03da86945
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2022-12-21 23:17:15 +0200

    am pus comentarii la ea

--- dictionary.c
+++ dictionary.c
@@ -4,12 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+// strcutura ce va contine dictionarul
 struct dictionary_entry {
     char *word;
     int priority;
 };
 
 // adaugam un cuvant in dictionar cu prioritatea prio
+// poz = pozitia pe care va fi adaugat in dictionar
 void add(struct dictionary_entry *d, char *cuvant, int poz, int prio) {
     d[poz].priority = prio;
     d[poz].word = malloc((strlen(cuvant) + 1) * sizeof(char));
@@ -19,19 +21,29 @@ void add(struct dictionary_entry *d, char *cuvant, int poz, int prio) {
 
 // cauti a in prima parte din b
 bool it_is_a_match(char *a, char *b) {
-    if (strlen(a) > strlen(b)) return false;
+    // daca primul are mai multe litere atunci nu se va afla in al doilea
+    if (strlen(a) > strlen(b)) {
+        return false;
+    }
+
+    // comparam caracter cu caracter
     for (int i = 0; i < strlen(a); ++i) {
-        if (a[i] != b[i]) return false;
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
 
-// nu face match cu niciunul din dictionar
-int in_dictionary(struct dictionary_entry *d, char *cuvant, int n) {
-    int poz = -1;
-    for (int i = 0; i < n; ++i) {
+// verificam daca un cuvant face match cu un cuvant din dictionar
+// n = numarul de cuvinte din dictionar
+int match_in_dictionary(struct dictionary_entry *d, char *cuvant, int n) {
+    int poz = -1, i;
+    for (i = 0; i < n; ++i) {
         if (it_is_a_match(cuvant, d[i].word)) {
             if (poz == -1 || d[poz].priority < d[i].priority) {
+                // daca este primul cuvant cu care face match sau am gasit unul
+                // cu prioritate mai mare
                 poz = i;
             } else {
                 if (d[poz].priority == d[i].priority &&
@@ -46,40 +58,42 @@ int in_dictionary(struct dictionary_entry *d, char *cuvant, int n) {
     return poz;
 }
 
-// int cmp(struct dictionary_entry *a, struct dictionary_entry *b) {
-//     if (strcmp((*a).word, (*b).word) > 0) return 1;
-//     if (strcmp((*a).word, (*b).word) < 0) return -1;
-//     return 0;
-// }
-
 int main() {
-    int n;  // cuvinte initial in dictionar
+    int n, m, i, j;
+    // n = numarul de cuvinte initial din dictionar
+    // m = numarul de cuvinte pentru autocomplete
     scanf("%d", &n);
-    char cuvant[21];
+    char cuvant[21];  // vectorul in care se citesc pe rand cuvintele
+
+    // declaram un pointer de tip dictionary_entry
     struct dictionary_entry *d = malloc(n * sizeof(struct dictionary_entry));
 
     // adaugam cuvintele in dictionar cu prioritatea 0
     for (int i = 0; i < n; ++i) {
+        // citim cuvintele de adaugat in dictionar
         scanf("%s", cuvant);
+
+        // adaugam cuvantele in dictionar
         add(d, cuvant, i, 0);
-        // printf("cuvant:%s   prio:%d\n", d[i].word, d[i].priority);
     }
 
-    int m;
     scanf("%d", &m);
 
-    for (int i = 1; i <= m; ++i) {
+    for (i = 1; i <= m; ++i) {
+        // citim cuvintele pentru autocomplete
         scanf("%s", cuvant);
 
-        // daca e semn de punctuatie
+        // daca e semn de punctuatie il afisam si citim in continuare
         if (strchr(",.:!?", cuvant[0])) {
             printf("%c ", cuvant[0]);
             continue;
         }
 
-        // verificam daca e de tip *
         if (cuvant[strlen(cuvant) - 1] != '*') {
-            int poz = in_dictionary(d, cuvant, n);
+            // daca nu e de tip cuvant*
+            // varificam daca face match in dictionar
+            int poz = match_in_dictionary(d, cuvant, n);
+
             if (poz == -1) {
                 // daca nu face match in dictionar il adaugam
                 printf("%s ", cuvant);
@@ -92,17 +106,19 @@ int main() {
                 printf("%s ", d[poz].word);
             }
         } else {
-            // daca e cu * la final punem \0 in locul stelutei
+            // daca e de tip cuvant* punem \0 in locul stelutei
             cuvant[strlen(cuvant) - 1] = '\0';
+            // verificam daca cuvantul se afla in dictionar
             bool ok = false;
-            for (int i = 0; i < n; ++i) {
-                if (strcmp(d[i].word, cuvant) == 0) {
+            for (j = 0; j < n; ++j) {
+                if (strcmp(d[j].word, cuvant) == 0) {
                     // daca cuvantul se afla in dictionar
-                    d[i].priority++;
+                    d[j].priority++;
                     ok = true;
                     break;
                 }
             }
+            
             // daca cuvantul nu se afla in dictionar il adaugam
             if (!ok) {
                 d = realloc(d, (n + 1) * sizeof(struct dictionary_entry));
@@ -115,11 +131,3 @@ int main() {
 
     return 0;
 }
-
-// qsort(d, n, sizeof(struct dictionary_entry), cmp);
-    // printf("\n");
-    // for (int i = 0; i < n; ++i) {
-    //     if(d[i].priority != 0)
-    //         printf("cuvant:%s   prio:%d\n", d[i].word, d[i].priority);
-    // }
-    // printf("%d\n", nr);
